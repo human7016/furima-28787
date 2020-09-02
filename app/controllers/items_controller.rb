158469ed9fe2d7e3e09bb1d73b29397fa3312db3
human7016 <on.82.hoshi.naohiro@gmail.com>
2020-09-02 17:08:25 +0900

    Active strageの導入

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -8,6 +8,12 @@ class ItemsController < ApplicationController
   end
 
   def create
+    Item.create(tweet_params)
   end
 
+  private
+
+  def item_paramas
+    params.require(:item).permit(:image, :name, :explanation, :price, :category, :quality, :delivery_fee, :shipping_area, :shipping_date).merge(user_id:current_user.id)
+  end
 end
