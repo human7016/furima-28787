93a0329e5278f346db5098877ffd756c12443e87
human7016 <on.82.hoshi.naohiro@gmail.com>
2020-09-02 19:10:51 +0900

    qualityクラスにActiveHash導入

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,6 +2,8 @@ class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   has_one_attached :image
   belongs_to_active_hash :category
+  belongs_to_active_hash :quality
 
   validates :category, numericality:{other_than:1}
+  validates :quality, numericality:{other_tahn:1}
 end
---
+++ app/models/quality.rb
@@ -0,0 +1,11 @@
+class Quality < ActiveHash::Base
+  self.data = [
+    {id:1, quality:'__'},
+    {id:2, quality:'新品、未使用'},
+    {id:3, quality:'未使用に近い'},
+    {id:4, quality:'目立った傷や汚れなし'},
+    {id:5, quality:'やや傷や汚れあり'},
+    {id:6, quality:'傷や汚れあり'},
+    {id:7, quality:'全体的に状態が悪い'}
+  ]
+end
