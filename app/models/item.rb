e42d77309faf704c82d83fee22f9214737b41e5a
human7016 <on.82.hoshi.naohiro@gmail.com>
2020-09-02 23:05:38 +0900

    その他itemモデルにActiveHash導入
    delivery_fee, shipping_area, shipping_dateモデルにActiveHashを導入

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,7 +3,15 @@ class Item < ApplicationRecord
   has_one_attached :image
   belongs_to_active_hash :category
   belongs_to_active_hash :quality
+  belongs_to_active_hash :delivery_fee
+  belongs_to_active_hash :shipping_area
+  belongs_to_active_hash :shipping_date
+
+  validates :user, :image, :name, :explanation, :price, :category, :quality, :delivery_fee, :shipping_area, :shipping_date, presence:true
 
   validates :category, numericality:{other_than:1}
   validates :quality, numericality:{other_tahn:1}
+  validates :delivery_fee, numericality:{other_tahn:1}
+  validates :shipping_area, numericality:{other_than:1}
+  validates :shipping_date, numericality:{other_tahn:1}
 end
