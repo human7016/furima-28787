b369c4df447fb18d98d8a40f5f893bf61771915c
human7016 <on.82.hoshi.naohiro@gmail.com>
2020-09-03 23:03:51 +0900

    コードレビューの修正1

--- app/models/item.rb
+++ app/models/item.rb
@@ -13,8 +13,8 @@ class Item < ApplicationRecord
   validates :price, numericality:{greater_than_or_equal_to:300, less_than:10000000}, format:{with:/\d/}
 
   validates :category_id, numericality:{other_than:1}
-  validates :quality_id, numericality:{other_tahn:1}
-  validates :delivery_fee_id, numericality:{other_tahn:1}
+  validates :quality_id, numericality:{other_than:1}
+  validates :delivery_fee_id, numericality:{other_than:1}
   validates :shipping_area_id, numericality:{other_than:1}
-  validates :shipping_date_id, numericality:{other_tahn:1}
+  validates :shipping_date_id, numericality:{other_than:1}
 end
