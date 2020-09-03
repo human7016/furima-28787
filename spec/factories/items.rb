4362d6cfee45bff776e864c95c98bdeee14321dc
human7016 <on.82.hoshi.naohiro@gmail.com>
2020-09-03 19:04:39 +0900

    FactoryBotの作成

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,13 +1,13 @@
 FactoryBot.define do
   factory :item do
-    price {Fker::number.within(range:300..9999999)}
+    price {Faker::Number.within(range:300..9999999)}
     name {Faker::Beer.name}
     explanation {Faker::Lorem.paragraph}
-    category_id {Faker::number.between(from:2, to:11)}
-    quality_id {Faker::number.between(from:2, to:7)}
-    delivery_fee_id {Faker::number.between(from:2, to:3)}
-    shipping_area_id {Faker::number.between(from:2, to:48)}
-    shipping_date_id {Faker::number.between(from:2, to:4)}
+    category_id {Faker::Number.between(from:2, to:11)}
+    quality_id {Faker::Number.between(from:2, to:7)}
+    delivery_fee_id {Faker::Number.between(from:2, to:3)}
+    shipping_area_id {Faker::Number.between(from:2, to:48)}
+    shipping_date_id {Faker::Number.between(from:2, to:4)}
     association :user
   end
 end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -2,13 +2,19 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   before do
-    @user = FactoryBot.build(:user)
+    @item = FactoryBot.build(:item)
+    @item.image = fixture_file_upload("public/images/test_image.png")
   end
 
-  describe '商品新規登録' do
-    context '新規登録がうまくいくとき' do
+  describe '商品登録' do
+    context '商品登録がうまくいくとき' do
       it "商品登録情報が全て存在すれば登録できる" do
-        expect(@user).to be_valid
+        expect(@item).to be_valid
+      end
+    end
+
+    context '商品登録がうまくいかないとき' do
+      it "商品画像がないと登録できない" do
       end
     end
   end
