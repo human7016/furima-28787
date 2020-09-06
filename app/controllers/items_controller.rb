90cdb2e480c043f546bccd1f2da22a3b95ef7f5b
human7016 <on.82.hoshi.naohiro@gmail.com>
2020-09-06 10:11:13 +0900

    updateアクションの作成

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -25,7 +25,14 @@ class ItemsController < ApplicationController
   end
 
   def update
+    item = Item.find(params[:id])
+    if item.update(item_params)
+      redirect_to root_path
+    else
+      render :edit
+    end
   end
+
   private
 
   def item_params
