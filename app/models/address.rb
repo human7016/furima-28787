49be29b9c4e3927576cef28847230d6628cd97e4
human7016 <on.82.hoshi.naohiro@gmail.com>
2020-09-07 18:21:40 +0900

    Addressモデルのバリデーション 記載

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,2 +1,10 @@
 class Address < ApplicationRecord
+  validates :postal_code, presence:true ,format:{with: /\A\d{3}[-]\d{4}\z/ }
+  validates :prefectures, presence:true
+  validates :city, presence:true, format:{with: /\A[ぁ-んァ-ン一-龥]+\z/ }
+  validates :address, presence:true
+  validates :building
+  validates :phone_number, presence:true, format:{with: /\A\d+[-]?\d+[-]?\d+\z/}
+  #電話番号はハイフンなしで保存
+  phone_number.gsub(/-/,'')
 end
